36b545120c43f748ed6eba4aa9d1c7145cdfc367
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-06-10 19:32:26 +0300

    succefully integrated using ubutu

--- printer/printer.py
+++ printer/printer.py
@@ -1,5 +1,18 @@
 from escpos.printer import Usb
+from datetime import datetime
 
-printer = Usb(0x0483, 0x5743, in_ep=0x81, out_ep=0x02)
+printer = Usb(0x0fe6, 0x811e, in_ep=0x82, out_ep=0x01)
+
 printer.text("Hello World\n")
+title = "Receipt: The Olmismis FCS"
+date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+farmer_name = "John Doe"
+farmer_number = "123456"
+
+# Print the receipt
+printer.set('center')  # Center align the text
+printer.text(f"{title}\n")
+printer.text(f"Date and Time: {date_time}\n")
+printer.text(f"Farmer Name: {farmer_name}\n")
+printer.text(f"Farmer Number: {farmer_number}\n")
 printer.cut()
